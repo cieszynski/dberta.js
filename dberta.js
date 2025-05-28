c2b16badce20cd48ddcd86113e82de95080c1190
cieszynski <admin@cieszynski.de>
2025-05-28 14:46:40 +0200

    read/write() added, begin() as closure

--- dberta.js
+++ dberta.js
@@ -63,57 +63,77 @@ Object.defineProperty(globalThis, 'dberta', {
                     }); // Object.entries(schema[v])
                 } // for loop
             } // onupgradeneeded
+
             request.onsuccess = (event) => {
                 console.debug('onsuccess');
                 const db = event.target.result;
 
+                const begin = (ronly = false, ...storeNames) => {
+                    let transaction;
+
+                    return new Promise(async (resolve, reject) => {
+                        try {
+                            transaction = db.transaction(storeNames, ronly ? 'readonly' : 'readwrite');
+
+                            resolve(storeNames.reduce((obj, storeName) => {
+                                const store = transaction.objectStore(storeName);
+
+                                obj[storeName] = {
+
+                                    execute(verb, ...args) {
+                                        return new Promise(async (resolve, reject) => {
+                                            try {
+                                                store[verb](...args).onsuccess = (event) => {
+                                                    resolve(event.target.result);
+                                                };
+                                            } catch (err) {
+                                                if (transaction) { transaction.abort(); }
+                                                reject(err);
+                                            }
+                                        });
+                                    },
+
+                                    add(obj, key) { return this.execute('add', obj, key); },
+
+                                    count(keyOrKeyRange) { return this.execute('count', keyOrKeyRange); },
+
+                                    delete(keyOrKeyRange) { return this.execute('delete', keyOrKeyRange); },
+
+                                    get(keyOrKeyRange) { return this.execute('get', keyOrKeyRange); },
+
+                                    getKey(keyOrKeyRange) { return this.execute('getKey', keyOrKeyRange); },
+
+                                    getAll(keyRange, limit) { return this.execute('getAll', keyRange, limit); },
+
+                                    getAllKeys(keyRange, limit) { return this.execute('getAllKeys', keyRange, limit); },
+
+                                    put(obj, key) { return execute('put', obj, key); },
+                                }
+
+                                return obj;
+                            }, {}));
+                        } catch (err) {
+                            if (transaction) { transaction.abort(); }
+                            reject(err);
+                        }
+                    });
+                }
+
                 resolve({
-
-                    close() {
-                        db.close();
-                    },
-
-                    begin(...storeNames) {
-                        let transaction;
-
-                        return new Promise(async (resolve, reject) => {
-                            try {
-                                transaction = db.transaction(storeNames, 'readwrite');
-
-                                resolve(storeNames.reduce((obj, storeName) => {
-                                    const store = transaction.objectStore(storeName);
-
-                                    obj[storeName] = {
-
-                                        execute(verb, ...args) {
-                                            return new Promise(async (resolve, reject) => {
-                                                try {
-                                                    store[verb](...args).onsuccess = (event) => {
-                                                        resolve(event.target.result);
-                                                    };
-                                                } catch (err) {
-                                                    if (transaction) { transaction.abort(); }
-                                                    reject(err);
-                                                }
-                                            });
-                                        },
-
-                                        add(obj, key) { return this.execute('add', obj, key); },
-
-                                        count(keyOrKeyRange) { return this.execute('count', keyOrKeyRange); },
-                                    }
-
-                                    return obj;
-                                }, {}));
-                            } catch (err) {
-                                if (transaction) { transaction.abort(); }
-                                reject(err);
-                            }
-                        });
-                    }
+                    write(...args) { return begin(false, ...args); },
+                    read(...args) { return begin(true, ...args); },
+                    close() { db.close(); },
                 });
-            }
+            } // request.onsuccess
+        }); // open
 
-        }) // open
+        // functions to build keyranges
+        eq = (z) => IDBKeyRange.only(z);
+        le = (y) => IDBKeyRange.upperBound(y);
+        lt = (y) => IDBKeyRange.upperBound(y, true);
+        ge = (x) => IDBKeyRange.lowerBound(x);
+        gt = (x) => IDBKeyRange.lowerBound(x, true);
+        between = (x, y, bx, by) => IDBKeyRange.bound(x, y, bx, by);
+        startsWith = (s) => IDBKeyRange.bound(s, s + '\uffff', true, true);
     }
-})
+}); // Object.defineProperty
